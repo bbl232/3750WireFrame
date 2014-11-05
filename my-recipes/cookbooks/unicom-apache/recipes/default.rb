820c824929e48ea46806d4c41d08aa09ead2d7a2
Bill Vandenberk <bill@vandenberk.me>
2014-11-05 10:08:21 -0500

    Chef recipe is working now

--- my-recipes/cookbooks/unicom-apache/recipes/default.rb
+++ my-recipes/cookbooks/unicom-apache/recipes/default.rb
@@ -1,10 +1,29 @@
 package 'apt'
 package 'apache2'
 
-cookbook_file "/etc/apache/sites-available/appleseed.conf" do
+directory "/vagrant" do
+    owner "vagrant"
+    group "vagrant"
+    mode "0666"
+    action :create
+end
+
+cookbook_file "/etc/apache2/sites-available/appleseed.conf" do
     source "appleseed.conf"
     owner "root"
     group "root"
     mode "0640"
+end
+
+execute "a2ensite appleseed.conf" do
+    user "root"
+end
+
+execute "a2dissite 000-default.conf" do
+    user "root"
+end
+
+execute "service apache2 reload" do
+    user "root"
 end
 

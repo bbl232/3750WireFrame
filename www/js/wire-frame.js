4ac73faf4b7723d74a5959c5a0ada77d1a753c15
Bill Vandenberk <bill@vandenberk.me>
2014-11-06 10:35:12 -0500

    tweaking the home page a bit.

--- www/js/wire-frame.js
+++ www/js/wire-frame.js
@@ -38,13 +38,14 @@ function logout(){
 function makeImage()
 {
     var div = document.getElementById('homepage_image');
+    var panelBelow = document.getElementById('about-us-panel');
     var img = new Image();
 
     img.onload = function(){
         div.appendChild(img);
     }
 
-    img.src = "http://placekitten.com/g/"+(div.offsetWidth)+"/350";
+    img.src = "http://placehold.it/"+(panelBelow.offsetWidth)+"x250&text=Appleseed Collective Image Banner";
 }
 
 function setupNavbar(){

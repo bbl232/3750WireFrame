451eeb152ca88be12cd3a9349288b7795a3192c2
areuillo <areuillo@mail.uoguelph.ca>
2014-11-11 17:45:55 -0500

    Added Staff Login code

--- www/js/wire-frame.js
+++ www/js/wire-frame.js
@@ -45,6 +45,16 @@ function login(){
     addEvent('wvandenb','123 Fake St','2014-11-11','13:30', '3h0m', 10, [{'Apple':2}, {'Cherry':1}]);
 }
 
+function staffLogin(){
+    var sid = document.getElementById('staff-login-email');
+    var spa = document.getElementById('staff-login-password');
+    if(sid.value != "" && spa.value != ""){
+        document.cookie = "User_id_appleseed="+sid.value;
+        document.cookie = "Staff_id_appleseed=staff";
+        window.location = window.location;
+    }
+}
+
 function register(){
     var upa = document.getElementById('login-password');
     var upc = document.getElementById('login-confirm_password');
@@ -59,15 +69,17 @@ function register(){
 
 function logout(){
     document.cookie = "User_id_appleseed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    document.cookie = "Staff_id_appleseed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
     document.cookie = "Appleseed_events=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
     window.location = "/index.php";
 }
 
 function setupNavbar(){
     var user_id = getCookie("User_id_appleseed");
+    var staff_id = getCookie("Staff_id_appleseed");
     var div = document.getElementById("nav-right");
     var div_left = document.getElementById("nav-left");
-    if(user_id==""){
+    if(user_id=="" && staff_id==""){
         div.innerHTML = '<li><form class="navbar-form" role="login"> \
           <div class="form-group"> \
             <input type="text" class="form-control" placeholder="E-mail" id="login-email" required> \
@@ -76,6 +88,10 @@ function setupNavbar(){
           <button type="submit" class="btn btn-primary" onclick="login()">Log In</button> \
           <button type="button" class="btn btn-default" id="popover-signup" data-container="body" data-toggle="popover" data-placement="bottom" data-html="true" data-content="<div class=\'input-group\'><input size=\'10\' type=\'password\' placeholder=\'Confirm Password\' id=\'login-confirm_password\' class=\'form-control\'><span class=\'input-group-btn\'><button onclick=\'register()\' type=\'button\' class=\'form-control btn btn-default\'>Register</button></span></div>">Create Account</button> \
         </form></li>';
+    }
+    else if(staff_id=="staff"){
+        div_left.innerHTML = div_left.innerHTML + "<li class='dropdown'><a href='#' class='dropdown-toggle' data-toggle='dropdown'>Actions <span class='caret'></span></a><ul class='dropdown-menu' role='menu'><li><a href='/volunteer.php'>Volunteer</a></li><li><a href='/trees.php'>Trees</a></li><li><a href='/myaccount.php'>My Account</a></li><li><a href='/viewFeedback.php'>View Feedback</a></li></ul></li>";
+        div.innerHTML = '<li><p class="navbar-text">Logged in as Staff '+user_id+'</p></li><li><form class="navbar-form"><button class="btn btn-default" onclick="logout()">Log Out</button></form></li>';
     }
     else{
         div_left.innerHTML = div_left.innerHTML + "<li class='dropdown'><a href='#' class='dropdown-toggle' data-toggle='dropdown'>Actions <span class='caret'></span></a><ul class='dropdown-menu' role='menu'><li><a href='/volunteer.php'>Volunteer</a></li><li><a href='/trees.php'>Trees</a></li><li><a href='/myaccount.php'>My Account</a></li></ul></li>";

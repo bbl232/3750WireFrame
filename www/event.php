0a7b52521a0881538d1b3253256fe6301ded9a5f
Mathieu Boutelier <mbouteli@mail.uoguelph.ca>
2014-11-11 12:33:45 -0500

    event page added

---
+++ www/event.php
@@ -0,0 +1,21 @@
+<?php
+	require("includes/header.php");
+?>
+		<div class="container">
+		<div class='row' id="events">
+			<div class='heading'>
+				<div class='pull-left'>
+					<h3>Planned Events</h3>
+				</div>
+				<div class='pull-right'>
+					<button type="button" class="btn btn-success">Add New Event</button>
+				</div>
+			</div>
+			<table class='table'>
+				<tr><td>Event Here</td></tr>
+			</table>
+		</div>
+	</div>
+<?php
+	require("includes/footer.php");
+?>

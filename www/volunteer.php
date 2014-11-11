1273690f562e30f3bf3d8ddf890f88381c512a78
wmaher <wmaher@mail.uoguelph.ca>
2014-11-11 18:50:10 -0500

    event changes

--- www/volunteer.php
+++ www/volunteer.php
@@ -1,6 +1,14 @@
 <?php
 require("includes/header.php");
+require("eventsList.php");
 ?>
+
+
+<p>Date: <input onchange="filter_list('date')" type="text" id="datepicker"></p>
+<script>
+ $( "#datepicker" ).datepicker();
+</script>
+
     
 
 

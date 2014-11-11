f0b2ab34acbabbb49aaef3c08cd0591dc7f13766
Mathieu Boutelier <mbouteli@mail.uoguelph.ca>
2014-11-11 13:39:49 -0500

    updated event list looks

--- www/event.php
+++ www/event.php
@@ -12,7 +12,14 @@
 				</div>
 			</div>
 			<table class='table'>
-				<tr><td>Event Here</td></tr>
+				<thead>
+					<tr><th>Fri, 21 Nov 2014</th><th>Time: 7:00AM</th></tr>
+				</thead>
+				<tbody>
+					<tr><td>Location: address</td><td>Producer: Name</td></tr>
+					<tr><td>Time Needed: 1:00 hour</td><td>Volunteers: 2/10</td></tr>
+					<tr><td>Nbr Trees: 20</td><td>Type of trees: Apple</td></tr>
+				</tbody>
 			</table>
 		</div>
 	</div>

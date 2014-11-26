de1ffdeb336d89975ffb0abf4d33ba2774e4e331
Isaac Horvath <isaac.horvath@gmail.com>
2014-11-26 00:29:05 -0500

    Logout now functional

--- www/js/wire-frame.js
+++ www/js/wire-frame.js
@@ -319,9 +319,14 @@ function register(){
 	logout() - log the user out, reset cookies, and refresh the page
 */
 function logout(){
+	var cookie = getCookie("Appleseed_user_details");
+	var parsed = JSON.parse(cookie);
 	$.ajax({
 		type: "POST",
 		url: "http://127.0.0.1:3000/users/current/logout",
+		beforeSend: function (request) {
+			request.setRequestHeader("Authorization", "AppleSeed token="+parsed['token']);
+		},
 		success: function(json) {
 			document.cookie = "User_id_appleseed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 			document.cookie = "Staff_id_appleseed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
